e7a805ad445b0948ecac6085ceadda1ca6afc253
hope1213 <1213hope@gmail.com>
2022-02-08 00:08:02 +0900

    403Forbidden 에러 수정

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/configuration/SecurityConfig.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/configuration/SecurityConfig.java
@@ -14,9 +14,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
 
     }
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
