2fd91a4ab9eac6c3c5d3332c5b044356cbff4120
hope1213 <1213hope@gmail.com>
2022-02-09 02:14:46 +0900

    cart도메인 추가

---
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/domain/Cart.java
@@ -0,0 +1,25 @@
+package com.example.MiniShop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Cart {
+    @Id @GeneratedValue
+    @Column(name = "cart_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> cartItem = new ArrayList<>();
+
+}
---
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/domain/CartItem.java
@@ -0,0 +1,26 @@
+package com.example.MiniShop.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class CartItem {
+
+    @Id @GeneratedValue
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+}
