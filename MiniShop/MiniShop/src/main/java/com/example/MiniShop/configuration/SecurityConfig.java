60cdcf7c260079ce1ed8b511c5bb493a3ae69a36
hope1213 <1213hope@gmail.com>
2022-02-08 17:56:02 +0900

    비밀번호 암호화 및 로그인 로그아웃

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/configuration/SecurityConfig.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/configuration/SecurityConfig.java
@@ -1,24 +1,46 @@
 package com.example.MiniShop.configuration;
 
+import com.example.MiniShop.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity // 웹보안 활성화를 위한 어노테이션
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    MemberService memberService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-
+        http.formLogin()
+                .loginPage("/member/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("userid")
+                .failureUrl("/member/login/error")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                .logoutSuccessUrl("/")
+        ;
     }
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){  // 비밀번호 암호화
         return new BCryptPasswordEncoder();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
     }
 }
