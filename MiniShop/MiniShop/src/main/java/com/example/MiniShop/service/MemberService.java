35aa14163d71960edc62eaa893f6a98344d5c473
hope1213 <1213hope@gmail.com>
2022-02-08 17:57:14 +0900

    userDtails에 오버라이드하여 로그인 기능 추가

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/service/MemberService.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/service/MemberService.java
@@ -3,13 +3,18 @@ package com.example.MiniShop.service;
 import com.example.MiniShop.domain.Member;
 import com.example.MiniShop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -19,9 +24,26 @@ public class MemberService {
     }
 
     private void validateDuplicateMember(Member member) {
-        Member findMember = memberRepository.findByEmail(member.getEmail());
+        Member findMember = memberRepository.findByUserid(member.getUserid());
         if(findMember != null) {
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
     }
+
+    /****로그인 기능 (UserDetails 오버라이드)****/
+    @Override
+    public UserDetails loadUserByUsername(String userid) throws
+            UsernameNotFoundException {
+        Member member = memberRepository.findByUserid(userid);
+
+        if(member == null) {
+            throw new UsernameNotFoundException(userid);
+        }
+        return User.builder()
+                .username(member.getUserid())
+                .password(member.getPassword())
+                .roles(member.getMemberStatus().toString())
+                .build();
+    }
+
 }
