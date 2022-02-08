46e3716512dc5bdb56f204bd1a17087de988cc1b
hope1213 <1213hope@gmail.com>
2022-02-08 17:57:49 +0900

    로그인을 위한 finduserid 추가

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/repository/MemberRepository.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/repository/MemberRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByUserid(String userid);
     Member findByEmail(String email);
+
 }
