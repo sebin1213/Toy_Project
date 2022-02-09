9e9bdd21a5ceec42c8954d4e741b730291b04f8c
hope1213 <1213hope@gmail.com>
2022-02-10 03:15:04 +0900

    필요없는 필드 제거

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/form/ItemForm.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/form/ItemForm.java
@@ -1,21 +1,25 @@
 package com.example.MiniShop.controller.form;
 
+import com.example.MiniShop.domain.ItemImg;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class ItemForm {
-    @NotEmpty(message = "회원 이름은 필수 입니다")
-    private Long id;
-
+    @NotEmpty(message = "상품 이름은 필수 입력 값입니다")
     private String name;
 
+    @NotNull(message = "상품가격은 필수 입력 값입니다")
     private int price;
 
+    @NotNull(message = "재고는 필수 입력 값입니다.")
     private int stockQuantity;
+
+    private ItemImg itemImg;
 
     private String itemDetail;
 
