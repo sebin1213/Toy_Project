27f1efea0365ba6282b93570cffce9578ca55875
hope1213 <1213hope@gmail.com>
2022-02-14 00:43:37 +0900

    itemList 컨트롤러 틀잡기

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/ItemController.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/ItemController.java
@@ -1,9 +1,11 @@
 package com.example.MiniShop.controller;
 
 import com.example.MiniShop.controller.form.ItemForm;
+import com.example.MiniShop.controller.form.ItemListForm;
 import com.example.MiniShop.domain.Item;
 import com.example.MiniShop.repository.ItemRepository;
 import com.example.MiniShop.service.ItemService;
+import com.example.MiniShop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -34,5 +37,14 @@ public class ItemController {
         Item item = Item.createItem(itemForm.getName(),itemForm.getPrice(),itemForm.getStockQuantity(),itemForm.getItemDetail());
         itemService.create(item);
         return "item/createItem";
+    }
+
+    @GetMapping(value = "/shop/item/top")
+    public String itemListForm(Model model){
+        List<Item> items = itemService.findAllItem();
+        List<ItemListDto> collects = items.stream().map(item -> new ItemListDto(item.getName(),item.getPrice(),item.getItemDetail())).collect(Collectors.toList());
+//        List<ItemListDto> collects = items.stream().map(item -> new ItemListDto().collect(Collectors.toList());
+        model.addAttribute("items", collects);
+        return "item/topList";
     }
 }
