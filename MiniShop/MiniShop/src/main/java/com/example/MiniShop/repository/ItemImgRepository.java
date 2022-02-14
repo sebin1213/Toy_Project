656802a29f4908d1b5d4c091de0d8abe9bfbc205
hope1213 <1213hope@gmail.com>
2022-02-14 21:43:51 +0900

    상품 이미지 레파지토리 추가

---
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/repository/ItemImgRepository.java
@@ -0,0 +1,7 @@
+package com.example.MiniShop.repository;
+
+import com.example.MiniShop.domain.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository  extends JpaRepository<ItemImg, Long> {
+}
