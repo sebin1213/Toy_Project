c1a108b6b04e5dc5d33a6ff636bcfd254671258f
hope1213 <1213hope@gmail.com>
2022-02-14 21:43:22 +0900

    dto 및 form 추가

---
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/form/ItemImgForm.java
@@ -0,0 +1,12 @@
+package com.example.MiniShop.controller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemImgForm {
+    private String Name; //이미지 파일명
+
+    private String imgUrl; //이미지 조회 경로
+}
