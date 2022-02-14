1caacf46aafa15fe4d45b5a21e287be24ba8af27
hope1213 <1213hope@gmail.com>
2022-02-14 21:40:54 +0900

    item 컨트롤러 수정 ( itemimg 추가하기위해서)

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/ItemController.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/ItemController.java
@@ -1,9 +1,11 @@
 package com.example.MiniShop.controller;
 
 import com.example.MiniShop.controller.form.ItemForm;
-import com.example.MiniShop.controller.form.ItemListForm;
+import com.example.MiniShop.controller.form.ItemImgForm;
 import com.example.MiniShop.domain.Item;
+import com.example.MiniShop.domain.ItemImg;
 import com.example.MiniShop.repository.ItemRepository;
+import com.example.MiniShop.service.ItemImgService;
 import com.example.MiniShop.service.ItemService;
 import com.example.MiniShop.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
@@ -23,27 +27,37 @@ import java.util.stream.Collectors;
 public class ItemController {
 
     private final ItemService itemService;
+    private final ItemImgService itemImgService;
 
     @GetMapping(value = "/admin/item/new")
     public String itemForm(Model model){
+        model.addAttribute("errorMessageImg", "");
         model.addAttribute("itemForm", new ItemForm());
         return "item/createItem";
     }
+
     @PostMapping(value = "/admin/item/new")
-    public String createItem(@Valid ItemForm itemForm, BindingResult result){
+    public String createItem(@RequestParam("itemImgFileList")MultipartFile itemImgFileList,@Valid ItemForm itemForm, BindingResult result, Model model) throws Exception{
         if(result.hasErrors()){
             return "item/createItem";
         }
-        Item item = Item.createItem(itemForm.getName(),itemForm.getPrice(),itemForm.getStockQuantity(),itemForm.getItemDetail());
-        itemService.create(item);
+        if(itemImgFileList.isEmpty()){
+            model.addAttribute("errorMessageImg", "상품 이미지는 필수 입력 값 입니다.");
+            return "item/createItem";
+        }
+
+        Item item = new Item();
+        ItemImg itemImg = itemImgService.createImg(itemImgFileList);
+
+        itemService.create(item.createItem(itemForm.getName(),itemForm.getPrice(),itemForm.getStockQuantity(),itemForm.getItemDetail(),itemImg));
         return "item/createItem";
     }
 
     @GetMapping(value = "/shop/item/top")
     public String itemListForm(Model model){
         List<Item> items = itemService.findAllItem();
-        List<ItemListDto> collects = items.stream().map(item -> new ItemListDto(item.getName(),item.getPrice(),item.getItemDetail())).collect(Collectors.toList());
-//        List<ItemListDto> collects = items.stream().map(item -> new ItemListDto().collect(Collectors.toList());
+        List<ItemListDto> collects = items.stream().map(item -> new ItemListDto(item.getName(),item.getPrice(),item.getItemDetail(),
+                item.getItemImg().getImgUrl())).collect(Collectors.toList());
         model.addAttribute("items", collects);
         return "item/topList";
     }
