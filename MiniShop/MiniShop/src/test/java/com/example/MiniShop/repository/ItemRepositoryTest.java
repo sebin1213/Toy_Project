63fa1d4a9bb22b41a345f7afe60f2c28dfd161ac
hope1213 <1213hope@gmail.com>
2022-02-14 21:42:56 +0900

    test코드 추가

--- MiniShop/MiniShop/src/test/java/com/example/MiniShop/repository/ItemRepositoryTest.java
+++ MiniShop/MiniShop/src/test/java/com/example/MiniShop/repository/ItemRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.MiniShop.repository;
 
 import com.example.MiniShop.domain.Enum.ItemStatus;
 import com.example.MiniShop.domain.Item;
+import com.example.MiniShop.domain.ItemImg;
 import com.example.MiniShop.domain.QItem;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -24,33 +25,11 @@ class ItemRepositoryTest {
     @Autowired
     ItemRepository itemRepository;
 
-    @Test
-    void item_save_test() throws Exception{
-        Item item = Item.createItem("나시",50000,30,"상세정보입니다아아아앙");
-
-        Item savedItem = itemRepository.save(item);
-        assertEquals("나시", savedItem.getName());
-    }
-    @Test
-    @Rollback(false)
-    void createItemList() throws Exception{
-//        for(int i=1; i<=10; i++) {
-//            Item item = new Item("나시"+i,50000+i,30+i,"상세정보입니다아아아앙"+i);
-//            System.out.println(item);
-//            Item savedItem = itemRepository.save(item);
-//        }
-    }
-
-    @Test
-    void findByItemNmTest() throws Exception{
-//        this.createItemList();
-//        List<Item> itemList = itemRepository.findByName("나시1");
+//    @Test
+//    void item_save_test() throws Exception{
+//        Item item = Item.createItem("나시",50000,30,"상세정보입니다아아아앙");
 //
-//        if(itemList.size()>0){
-//            assertEquals("나시", "나시");
-//        }
-//        else{
-//            assertEquals("나시", "sktl");
-//        }
-    }
+//        Item savedItem = itemRepository.save(item);
+//        assertEquals("나시", savedItem.getName());
+//    }
 }
