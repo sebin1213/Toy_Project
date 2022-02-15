b94dfe819218a52a27a12e8c19a2d1fad62ec236
hope1213 <1213hope@gmail.com>
2022-02-16 01:24:54 +0900

    상품수정 로직 추가

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/service/ItemService.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/service/ItemService.java
@@ -1,13 +1,17 @@
 package com.example.MiniShop.service;
 
+import com.example.MiniShop.controller.Dto.ItemUpdateDto;
 import com.example.MiniShop.domain.Item;
 import com.example.MiniShop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ItemService {
 
@@ -18,5 +22,15 @@ public class ItemService {
     }
     public List<Item> findAllItem(){
         return itemRepository.findAll();
+    }
+    public Optional<Item> findById(Long id){
+        return itemRepository.findById(id);
+    }
+    public void updateItem(ItemUpdateDto itemUpdateDto){
+        Optional<Item> itemOptional = itemRepository.findById(itemUpdateDto.getId());
+        if (itemOptional.isPresent()){
+            Item item = itemOptional.get();
+            item.updateItem(itemUpdateDto);
+        }
     }
 }
