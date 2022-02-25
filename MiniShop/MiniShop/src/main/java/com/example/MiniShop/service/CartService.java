c5ebbbc4b03f8c5fde943e4ce767ffceeb95826e
hope1213 <1213hope@gmail.com>
2022-02-25 23:13:33 +0900

    장바구니 추가 service

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/service/CartService.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/service/CartService.java
@@ -1,18 +1,50 @@
 package com.example.MiniShop.service;
 
+import com.example.MiniShop.domain.Cart;
 import com.example.MiniShop.domain.CartItem;
+import com.example.MiniShop.domain.Item;
+import com.example.MiniShop.domain.Member;
 import com.example.MiniShop.repository.CartRepository;
+import com.example.MiniShop.repository.ItemRepository;
+import com.example.MiniShop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CartService {
     private final CartRepository cartRepository;
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
-    public CartItem addCart(CartItem cartItem){
-        return cartRepository.save(cartItem);
+    public Cart createCart(Cart cart){
+        return cartRepository.save(cart);
     }
+
+    public void addCart(String userId,Long itemId, int quantity){
+        Optional<Item> item_op = itemRepository.findById(itemId);
+        if (item_op.isPresent()){
+            Item item = item_op.get();
+            if (item.getStockQuantity() >= quantity) {
+                Member member = memberRepository.findByUserid(userId);
+                CartItem cartItem = CartItem.createCartItem(item, quantity);
+                Cart cart = member.getCart();
+                cart.addCartItem(cartItem);
+
+                createCart(cart);
+            }
+        }
+        else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    public List<CartItem> cartItemList(String userId){
+        return memberRepository.findByUserid(userId).getCart().getCartItems();
+    }
+
 }
