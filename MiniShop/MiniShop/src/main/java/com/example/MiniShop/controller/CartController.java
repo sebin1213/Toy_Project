52defed16f125434a7ced183913086378f55d057
hope1213 <1213hope@gmail.com>
2022-03-08 23:55:48 +0900

    삭제 컨트롤러 추가

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/CartController.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/controller/CartController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 import java.util.List;
@@ -19,10 +21,22 @@ public class CartController {
 
     @GetMapping(value = "/shop/cart")
     public String cartForm(Model model,Principal principal){
+
         List<CartItem> cartItems = cartService.cartItemList(principal.getName());
+
+
         List<CartItemDto> cartItemDtoList = cartItems.stream().map(cartItem -> new CartItemDto(cartItem.getItem().getId(),cartItem.getItem().getName(),cartItem.getCount(),
                 cartItem.getItem().getPrice(),cartItem.getItem().getItemImg().getImgUrl())).collect(Collectors.toList());
+
         model.addAttribute("itemList",cartItemDtoList);
+//        cartService.cartItemList(principal.getName());
+
         return "/cart/cartForm";
+    }
+
+    @GetMapping(value = "/shop/cart/{item_id}/delete")
+    public String cartDelete(Model model, @PathVariable("item_id") Long item_id,Principal principal){
+        cartService.deleteCart(item_id,principal.getName());
+        return "redirect:/shop/cart";
     }
 }
