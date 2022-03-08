104c237a58dc6c65360aed9e6d681162509e0552
hope1213 <1213hope@gmail.com>
2022-03-08 23:56:40 +0900

    장바구니 상품삭제 스프링데이터jpa

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/repository/CartRepository.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/repository/CartRepository.java
@@ -4,4 +4,6 @@ import com.example.MiniShop.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    @Override
+    void deleteById(Long aLong);
 }
