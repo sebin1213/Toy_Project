9ffb6066372997a6b672724954749d77c6a061ab
hope1213 <1213hope@gmail.com>
2022-03-08 23:56:02 +0900

    장바구니 상품추가 삭제 수정

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/domain/Cart.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/domain/Cart.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Entity
 @Getter @Setter
@@ -17,12 +18,27 @@ public class Cart {
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval=true) //orphanRemoval=true 고아 객체를 삭제해준다. 장바구니까 날림... 상품이면 날리면 안됨.....
     private List<CartItem> cartItems = new ArrayList<>(); // 왜 ArrayList?? 1:N 관계에서 N이 없는 경우 null인 상태인 보다 Empty가 훨씬 직관적이다.null의 경우 값을 못 가져온 것인지 값이 비어있는건지 애매하다.
+
+//    @OneToOne(mappedBy = "cart",fetch = FetchType.LAZY)
+//    private Member member;
     /**
     / 연관관계 메서드
      **/
     public void addCartItem(CartItem cartItem) {
-        cartItems.add(cartItem);
-        cartItem.setCart(this);
+        boolean existCartItem = false;
+        for (CartItem c_item: cartItems) {
+            if(c_item.getItem().getId()==cartItem.getItem().getId()){
+                c_item.addCountCartItem(cartItem.getCount());
+                existCartItem = true;
+            }
+        }
+        if(!existCartItem){
+            cartItems.add(cartItem);
+            cartItem.setCart(this);
+        }
     }
 
+    public void removeCartItem(CartItem cartItem){
+        cartItems.remove(cartItem);
+    }
 }
