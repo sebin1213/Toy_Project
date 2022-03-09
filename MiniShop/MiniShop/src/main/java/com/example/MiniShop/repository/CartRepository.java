406c625f90cb850b63890b6155e26baacf5881db
hope1213 <1213hope@gmail.com>
2022-03-09 21:52:21 +0900

    잘못넣은 delete문 제거

--- MiniShop/MiniShop/src/main/java/com/example/MiniShop/repository/CartRepository.java
+++ MiniShop/MiniShop/src/main/java/com/example/MiniShop/repository/CartRepository.java
@@ -4,6 +4,5 @@ import com.example.MiniShop.domain.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    @Override
-    void deleteById(Long aLong);
+
 }
